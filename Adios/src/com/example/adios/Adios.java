46de21b8e8222be40352dcab44e76af4c00be0b6
wurz44 <acuerdob@gmail.com>
2014-10-07 12:34:32 +0200

    initial commit

---
+++ Adios/src/com/example/adios/Adios.java
@@ -0,0 +1,15 @@
+package com.example.adios;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Adios extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adios);
+    }
+}
